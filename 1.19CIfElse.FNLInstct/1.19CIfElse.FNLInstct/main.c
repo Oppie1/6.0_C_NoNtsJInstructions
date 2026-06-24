263d0af268e34f9e17d84d5009df2e23edb19254
Oppie1 <atindall729@gmail.com>
2026-06-24 18:12:21 -0500

    Add files via upload

---
+++ 1.19CIfElse.FNLInstct/1.19CIfElse.FNLInstct/main.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+int main() {
+
+	//Declare an uninitialized int variable to hold the user's age.
+	//CODE:
+
+	//Declare a character variable to hold the user's gender. 
+	//CODE:
+
+	//call printf() function to prompt user to enter their age.
+	//CODE:
+	printf("How old are you?\n");
+
+	//call scanf_s() function to scan in the users age.
+	//CODE:
+
+	//Call printf() to request the users age.
+	//CODE:
+	printf("\nWhat is your gender m/f?\n");
+
+	//Read in a single character from the console and stores it in memory address of 'gender'.
+	//CODE:
+
+	//Use if condition to test if user is above 18 and print you may enter if they are.
+	//CODE:
+
+	//Use nested if/else below to determine whether person is a male or female.
+	//CODE:
+
+	//Make a nested else to the first if that executes and says a female has entered the website.
+	//CODE:
+
+	//Make an else condition that prints that the person is not old enough to enter the website.
+	//CODE:
+
+}
---
+++ 1.20CMorThn2Choics.FNLInstrct/1.20CMorThn2Choics.FNLInstrct/main.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+	//Declare three uninitialized float variables representing 3 grades.
+	//CODE:
+
+	//Prompt user to enter their 3 grades
+	//CODE:
+	printf("Enter your 3 grades:\n");
+
+	//Scan in the three grades from the user one at a time.
+	//CODE:
+
+	//Assign a new float var avg to the result of calculation using the 3 grades divided by 3. 
+	//CODE:
+
+	//Print a sentence to the screen that displays the average to 3 decimal places.
+	//CODE:
+
+	//Make if condition that tests if the average is greater than or = to 90. If so print out Grade A to the screen.
+	//CODE:
+
+	//Make else/if statement that tests if the average is greater than or equal to 80. If so print Grade B to the screen.
+	//CODE:
+
+	//Make another else/if statement that test if the average is greater than or equal to 70. If so then print Grade C to the screen.
+	//CODE:
+
+	//Make one more if/else statement with same criteria for Grade D (60)
+	//CODE:
+
+	//Use just a "else" statement as a catch all for all F (<60) grades and make a print
+	//Statement that says grade F.
+
+}
