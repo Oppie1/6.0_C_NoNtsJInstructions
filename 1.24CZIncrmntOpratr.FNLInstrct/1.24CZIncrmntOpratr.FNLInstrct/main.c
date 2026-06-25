278d4e7f0ad82cc8cf1ecc515133bb58c444ce79
Oppie1 <atindall729@gmail.com>
2026-06-24 22:27:39 -0500

    Add files via upload

---
+++ 1.24CZIncrmntOpratr.FNLInstrct/1.24CZIncrmntOpratr.FNLInstrct/main.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+	//Declare 3 int variables.  a, b, answer
+	//be initialized to 5 and 10 respectively. answer should be initialized to 0.
+	//CODE:
+
+	//Assign answer to the expression incrementing a before it is multiplied by b.
+	//CODE:
+
+	//Call printf() with a and 'answer' with a sentence to print a and answer to the screen. 6 and 60
+	//CODE:
+
+	//Declare variables to the original values 
+	//CODE:
+
+	//Assign answer set to increment a after and then multiply by b.
+	//CODE:
+
+	//Call printf() with a and answer with sentence to the screen to show difference with pre increment. 6 and 50
+	//CODE:
+
+	printf("\n------Next Demonstration-------\n\n");
+
+	//Declare an initialized variable named tuna and set it to 20
+	//CODE:
+
+	//Display the initial value of to the screen
+	//CODE:
+
+	//Use pre-increment operator so tuna goes from 20 to 21.
+	//CODE:
+
+	//Now show the new value to the screen.
+	//CODE:
+
+	//Use the pre-increment again using tuna and printf() to screen.
+	//CODE:
+	
+	//Do the subtraction increment operator -- just to show in example proficiency. And print to screen. 
+	//CODE:
+
+	//ASSIGN tuna and to the pre-increment of tuna * 2.
+	//CODE:
+
+	//ASSIGN tuna and then tuna to the post-increment operator to see what happens.
+	//Multiply by 2 like above.
+	//CODE:
+
+}
---
+++ 1.25CWhileLoops.FNLInstrct/1.25CWhileLoops.FNLInstrct/main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//Make a program that starts out at .10 cents and doubles every day for 10 days to see how much money you would
+//get on day 10. Make an expression that takes the amount on a given day and multiplies it by 2 and then moves
+//to the increment step to move to the next day before going back to the top while loop that will keep running
+//so long as day<10. The program prints one line during each iteration until the 10th iteration where 
+//day = 10 -> 10 < 10? False, loop stops.
+
+int main()
+{
+	//Declare an int variable for the loop counter named day an initialize it to 1.
+	//CODE:
+
+	//Declare a float variable money that stores a decimal that represents the starting value of .10 cents
+	//CODE:
+
+	//Make a while loop that test for the day so long as it is less than 10.
+	//CODE:
+
+	//Use print function to show which day %d the loop is on and then the current value of amount on that day.
+	//CODE:
+
+	//Assign variable money to an expression that doubles the amount. Remember you can use *= as shorthand for this step. 
+	//CODE:
+
+	//Increment the day by 1
+	//CODE:
+
+}
