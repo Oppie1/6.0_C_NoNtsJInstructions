c8aad658bd0d4dd9f590ad5fc740e93c0dbaf9a9
Oppie1 <atindall729@gmail.com>
2026-06-24 22:25:33 -0500

    Add files via upload

---
+++ 1.21CStdyHrsNPorBhavir.FNLInstrct/1.21CStdyHrsNPorBhavir.FNLInstrct/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+	//Declare two uninitialized int variables to hold values for hours a student studied and bad behavior
+	//CODE:	
+
+	//call a printf() function to ask user how many hours they studied. 
+	//CODE:
+	printf("How many hours did you study?\n");
+	//call a scanf_s() function to scan in the information from the user input. &
+	//CODE:
+
+	//Use same logic calls to prompt and scan in user bad behavior data.
+	//CODE:
+	printf("Did you get a mark for bad behavior did you get? 1 = yes 0 = no\n");
+
+	//Use if statement with two controls to check if hours studied are more than 10 hours AND
+	//another to see if they got marks for bad behavior.
+	//CODE:
+	
+}
