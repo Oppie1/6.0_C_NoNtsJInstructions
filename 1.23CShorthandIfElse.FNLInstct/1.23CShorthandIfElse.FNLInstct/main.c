8e06622c25272c499dcc4012802a75cab3510c30
Oppie1 <atindall729@gmail.com>
2026-06-24 22:26:37 -0500

    Add files via upload

---
+++ 1.23CShorthandIfElse.FNLInstct/1.23CShorthandIfElse.FNLInstct/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+/*
+Write a program that provides 20 characters buffer space for last name. Ask a persons
+//last name. 1st letter less than 'M' () () ()
+*/
+
+int main() {
+
+
+	//Declare an uninitialized character array lastName that has 20 elements
+	//CODE:
+
+	//Prompt user to enter their last name.
+	//CODE:
+	printf("Please enter your last name.");
+
+	//Call a scanf_s() function with 3 parameters to see in the users last name. sizeof()
+	//CODE:
+	
+
+	//Use the ternary operator to determine if first letter of the persons lastName FIRST ELEMENT is < M 
+	//REMEMBER->:?''"" No if.  (_<M) printf(color) ()
+	//CODE:
+	
+}
