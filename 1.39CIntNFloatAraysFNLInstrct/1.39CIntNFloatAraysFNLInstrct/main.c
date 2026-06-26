cdff1c41383c81c8d0f6a772b8c8b407198b9b2d
Oppie1 <atindall729@gmail.com>
2026-06-25 23:04:54 -0500

    Add files via upload

---
+++ 1.39CIntNFloatAraysFNLInstrct/1.39CIntNFloatAraysFNLInstrct/main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+
+
+int main() {
+
+	//Declare an uninitialized int var for counter.
+	//CODE:
+
+	//Declare int array meatBalls with 5 uninitialized elements.
+	//CODE:
+
+	//Declare int var called totalMeatBalls initialized 0
+	//CODE:
+
+	//Use a for loop initialize i to 0, the less than 5 and increment by 1
+	//CODE:
+
+	//Call printf() to ask user how many meatballs they ate on day i (+1 since i 0).
+
+	//Call scanf_s() to scan in the user input for EACH day.
+
+	//Assign "totalMeatBalls" to expression of each user entry.
+	//CODE:
+
+	//Declare initialized int var avg that will be assigned a value from total meatballs and divided by i.
+	//CODE:
+
+	//Print to screen a sentence that matches up with  totalMeatBalls as well as avg respectively in each of the two arguments.
+	//CODE:
+
+}
