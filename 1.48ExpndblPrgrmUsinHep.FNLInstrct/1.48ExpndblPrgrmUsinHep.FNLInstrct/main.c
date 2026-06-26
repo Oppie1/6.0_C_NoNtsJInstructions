952f4f87595eaee9333cb6926a44025387223598
Oppie1 <atindall729@gmail.com>
2026-06-26 18:59:56 -0500

    Add files via upload

---
+++ 1.48ExpndblPrgrmUsinHep.FNLInstrct/1.48ExpndblPrgrmUsinHep.FNLInstrct/main.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main() {
+
+	//Declare uninitialized int var for loop counter as well as a int
+	//variable to store how many numbers user wants to average
+	//CODE:
+
+	//Declare int var named total and initialize it to 0.
+	//CODE:
+
+	//Declare a float variable named average  initialized 0.
+	//CODE:
+
+	//Declare uninitialized int pointer named pointsArray.
+	//CODE:
+
+	printf("\nHow many numbers do you want to average?\n");
+
+	//Call scanf_s() to bring info into the memory of howMany.
+	//CODE:
+
+	//Assign pointsArray int pointer to expression that will request memory from heap at 
+	//runtime using typecast and malloc(like last tut but wth var)
+	//CODE:
+
+	printf("\nGo ahead and answer those numbers.");
+
+	//Construct for loop that loops through user entries with counter 
+	//starting at 0, comparing counter to howMany and increment it by 1.
+	//CODE:
+
+		//Scan in user chosen numbers into the memory location of the given element of the pointsArray.
+		//CODE:
+
+		//Assign total to expression that takes value entered in points array elements and adds it to the current value..
+		//CODE:
+
+	//Assign var average to result of total divided by howMany numbers the user entered. typecast.
+	//CODE:
+
+	//Call printf() to display the average to the screen.
+	//CODE:
+}
---
+++ 1.49Structures.FNLInstrcts/1.49Structures.FNLInstrcts/main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Include the header file for employee info so the struct definition is available in this file. ""
+//CODE:
+#include "EmployeeInfo.h";
+
+
+int main() {
+
+	//Declare two variables of type struct user -> copilot and claude.
+	//CODE:
+
+	//ASSIGN copilot and claude user id, age and weight to decimal.
+	//CODE:
+
+	//Use a puts() to prompt user for name of user 1
+	//CODE:
+	puts("What is the user 1's name\n");
+
+	//Use a gets() to read in information from struct copilot -> fullName (.)
+	//CODE:
+
+	//Use a puts() to prompt user for name of user 2
+	//CODE:
+	puts("What is the user 2's name\n");
+
+	//Use gets() to read in information from struct claude -> firstName.
+	//CODE:
+
+	//Use two printf() to display information to screen for both copilot and claude. (.)
+	//CODE:
+
+
+}
