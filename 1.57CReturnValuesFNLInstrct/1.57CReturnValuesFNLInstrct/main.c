e02ee684e82ead5d4df5a41761b4bef40aff47a4
Oppie1 <atindall729@gmail.com>
2026-06-28 15:00:34 -0500

    Add files via upload

---
+++ 1.57CReturnValuesFNLInstrct/1.57CReturnValuesFNLInstrct/main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//Declare function prototype with int data type calculateBonus with
+//which takes int yearsWorked as its parameter.
+//CODE:
+
+int main() {
+
+	//Declare int var kanopisBonus initialized to the function calculate bonus with 14 as arg.
+	//CODE:
+
+	//Declare and initialize int var petraBonus equal to function calculate bonus with 3 as arg.
+	//CODE:
+
+	//Print Kanopi's bonus to the screen.
+	//CODE:
+
+	//Print Petra's bonus to the screen.
+	//CODE:
+
+}
+
+//Define calculate bonus function,
+//CODE:
+
+	//Declare and assign int var bonus to show what bonus is for years worked by 250
+
+	//Use nested if statement with condition yearsWorked is greater than 10 they get 1000 more dollars.
+	//CODE:
+
+	//Use return variable bonus to return final bonus to call function in main.
+	//CODE:
---
+++ 1.58CZPasByRef.FNLInstrct/1.58CZPasByRef.FNLInstrct/main.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+//Declare void function passByValue with uninitialized int parameter i.
+//CODE:
+
+//Declare void function passByAddress() with uninitialized int pointer * parameter  i.
+//CODE:
+
+int main() {
+
+	//Declare int variable named tuna initialized it to 20.
+	//CODE:
+
+	//Call passByValue function with tuna as parameter.
+	//CODE:
+
+	//Call a printf() that says passing by value did not change variable in main().
+	//CODE:
+
+	//Call the passByAddress() with memory address of the variable tuna as its parameter
+	//CODE:
+
+	//Call printf() that says passing by address did change the variable in main().
+	//CODE:
+
+}
+
+//Define the void passByValue function
+//CODE:
+
+//Initialize i to 37.
+//CODE:
+
+//Call a printf function to make sentence saying inside passByValue i is now...
+//CODE:
+
+//Define the void passByAddress function.
+//CODE:
+
+//Initialize pointer i to 64. 
+//CODE:
