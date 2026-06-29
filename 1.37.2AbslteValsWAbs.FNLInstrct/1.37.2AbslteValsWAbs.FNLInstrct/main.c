4f0733e76ecdba0054defb49bba301b7ba940c56
Oppie1 <atindall729@gmail.com>
2026-06-29 15:51:59 -0500

    Add files via upload

---
+++ 1.37.2AbslteValsWAbs.FNLInstrct/1.37.2AbslteValsWAbs.FNLInstrct/main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+int main() {
+
+	//Set up three uninitialized int vars: one birth year, one current year and calculated age.
+	//CODE:
+
+	//Ask user to provide year of their birth.
+	//CODE:
+	printf("What year were you born?\n");
+
+	//Capture user's birth year from console using scanf_s.
+	//CODE:
+
+	//Ask user to provide current year.
+	//CODE:
+	printf("\nWhat is the current year?\n");
+
+	//Capture the current year from the console using scanf_s().
+	//CODE:
+
+	//Subtract birth year from current year to find and assign age.
+	//CODE:
+
+	//Call print function to show raw year person was born.	
+	//CODE:
+
+	//Fix potential negative ages by assigning age to result of abs() with appropriate arg.
+	//CODE:
+
+	//Print out the corrected, strictly-positive age to the console.
+	//CODE:
+
+}
